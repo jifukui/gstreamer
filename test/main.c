019f35323df794abcc691db50128afc5e3025c8a
jifukui <805013833@qq.com>
2019-06-18 11:10:24 +0800

    test

---
+++ test/main.c
@@ -0,0 +1,20 @@
+#include<gst/gst.h>
+int main(int argc,char **argv)
+{
+    GstElement *pipeline=NULL;
+    GstBus *bus=NULL:
+    GstMessage *msg;
+    gst_init(&argc,argv);
+    pipeline=gst_parse_launch("playbin2 uri=http://docs.gstreamer.com/media/sintel_tariler-480p.webm",NULL);
+    gst_element_set_state(pipeline,GST_STATE_PLAYING);
+    bus=gst_element_get_bus(pipeline);
+    msg=h=gst_bus_timed_pop_filtered(bus,GST_CLOCK_TIME_NONE,GST_MESSAGE_ERROR|GST_MESSAGE_EOS);
+    if(msg!=NULL)
+    {
+        gst_message_unref(msg);
+    }
+    gst_object_unref(bus);
+    gst_element_set_state(pipeline,GST_STATE_NULL);
+    gst_object_unref(pipline);
+    return 0;
+}
